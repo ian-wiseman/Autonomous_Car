4a42fac47d5298d819d49dd3a29cbc263c1327bb
Ian Wiseman <68253726+ian-wiseman@users.noreply.github.com>
2021-03-11 00:43:03 -0500

    Update computer-vision.ts

--- computer-vision.ts
+++ computer-vision.ts
@@ -1,3 +1,8 @@
+/*
+Function which determines the likelihood of the car encountering an
+obstacle on the left or right side. There is a 50/50 chance of 
+either outcome
+*/
 export function getObstacleEvents() {
     const coinFlip = Boolean(Math.random() > 0.5);
     return { 
